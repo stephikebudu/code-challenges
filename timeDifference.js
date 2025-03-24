358f316a47967c54412b4e225690001faf3f14de
Stephany Ikebudu <stephikebudu@gmail.com>
2025-03-24 15:44:36 +0100

    fix(minor): arrange syntax for better view during live stream

--- timeDifference.js
+++ timeDifference.js
@@ -1,25 +1,28 @@
 /*
   PROBLEM:
-  Create a function which accepts an `alarm` parameter. This will be the time our
-  alarm to wake up has been set. The function should then calculate the difference 
-  between the current time and the alarm time. Return that difference as a string 
-  in the format `${hours} hours and ${minutes} minutes of sleep`. For example, 
-  "3 hours and 45 minutes of sleep". Constraints: There are about 1000 different 
-  ways to write time. You could use a Date object, or an "HH:MM" string, is it 12 hour 
-  time or 24 hour time? You choose! You may choose whichever input format you would like 
-  to require for the `alarm` parameter. HOWEVER you MUST validate that input format - that is, 
-  if your function expects a 12 hour time, and I do not indicate AM/PM in my argument, the function 
+  Create a function which accepts an `alarm` parameter. This will be
+  the time our alarm to wake up has been set. The function should then
+  calculate the difference between the current time and the alarm time.
+  Return that difference as a string in the format `${x} hours and ${y}
+  minutes of sleep`. For example, "3 hours and 45 minutes of sleep".
+  Constraints: There are about 1000 different ways to write time. You
+  could use a Date object, or an "HH:MM" string, is it 12 hour time or
+  24 hour time? You choose! You may choose whichever input format you
+  would like to require for the `alarm` parameter. HOWEVER you MUST
+  validate that input format - that is, if your function expects a 12
+  hour time, and I do not indicate AM/PM in my argument, the function
   should exit with feedback about the exact format you are expecting
 */
 
 /*
 PSEUDOCODE:
 - Accept time to wake up in 24hour string format
-- Check if input format is string, check if HH is between 0-23, check if MM is between 0-59, check if 
-format is 24hours (ie no AM or PM attached). Use regex
+- Check if input format is string, check if HH is between 0-23, check
+if MM is between 0-59, check if format is 24hours (ie no AM or PM attached). Use regex
 - Convert input to number type, convert to minutes
 - Get the current hour and minute, convert to number, convert to minutes
-- Get the difference, get the hour(s) and minute(s) in the difference and return with the string
+- Get the difference, get the hour(s) and minute(s) in the difference 
+and return with the string
 */
 
 function timeDifference(alarmTime) {
@@ -55,5 +58,5 @@ function timeDifference(alarmTime) {
 
 console.log(timeDifference("10:55"));
 console.log(timeDifference("14:20"));
-console.log(timeDifference("08:57"));
-console.log(timeDifference("10:30"));
+console.log(timeDifference("13:01"));
+console.log(timeDifference("13:55"));
