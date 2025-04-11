055470b16d4ab10c2148cb34e0d50d8c0acd5184
Stephany Ikebudu <stephikebudu@gmail.com>
2025-04-11 22:13:35 +0100

    debug encrypt function

--- encrypt.js
+++ encrypt.js
@@ -4,7 +4,8 @@ PSEUDOCODE
 2. call the function for each element of the word array starting from the current index of word
 */
 
-function encrypt(str) {
+// There is a bug in this version
+/*function encrypt(str) {
   const strArr = str.split(" ");
   const consonantRegex = /[b-df-hj-np-tv-z]/gi
   let newChar
@@ -25,13 +26,41 @@ function encrypt(str) {
 
 
   const encryptedStrArr = strArr.map((w) => {
-    console.log(w)
     return updateWord(w);
   })
   return encryptedStrArr.join(" ")
+} */
+
+
+  
+/*
+PSEUDOCODE
+1. verify input
+2. create consonant string manually
+3. loop through input string, with each character check if its a consonant and add "egg" if it is, if its not leave as is
+4. add (updated) character to new string variable
+5. finally when loop is done return new string variable
+*/
+
+// Debugged version
+function encrypt(str) {
+  if (typeof str !== "string") {
+    return "Type of input not allowed, enter a string enclosed in single or double quotation marks"
+  }
+
+  let encrypted = "";
+  const consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    encrypted += char;
+    if (consonants.includes(char)) {
+      encrypted += "egg"
+    }
+  }
+  return encrypted;
 }
 
 console.log(encrypt("Hello to the freeCodeCamp Discord"));
 console.log(encrypt("Steph needs to come up with harder challenges"));
 console.log(encrypt("Maybe if she spent less time on TikTok she could"));
-console.log(encrypt("To her"));
