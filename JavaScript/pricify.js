974c798754b21a8940ea17a742dfe82e9d964021
Stephany Ikebudu <stephikebudu@gmail.com>
2025-05-30 16:20:23 +0100

    develop algorithm for pricify challenge

--- JavaScript/pricify.js
+++ JavaScript/pricify.js
@@ -9,3 +9,19 @@ else if cent amount is greater than 2 AND less than 8
 else
   round price up
 */
+
+function pricify(price){
+  const dollarAmount = Math.floor(price);
+  let centAmount = (price - dollarAmount).toFixed(1);
+  let newPrice;
+
+  if (centAmount < 0.3){
+    newPrice = dollarAmount;
+  } else if (centAmount > 0.2 && centAmount < 0.8) {
+    newPrice = dollarAmount + 0.5;
+  } else {
+    newPrice = Math.ceil(price)
+  }
+
+  return newPrice;
+}
