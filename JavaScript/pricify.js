060220da656e14918bd3287d7fc4efdb3e6bf5a7
Stephany Ikebudu <stephikebudu@gmail.com>
2025-05-30 16:38:03 +0100

    update pseudocode for accuracy

--- JavaScript/pricify.js
+++ JavaScript/pricify.js
@@ -3,7 +3,7 @@ PSEUDOCODE
 separate cent amount from dollar amount by rounding down price to get dollar amount. subtract dollar amount from price and round down difference to 1 decimal place to get cent amount
 check if cent amount is less than 0.3
   then round price down
-else if cent amount is greater than 2 AND less than 8
+else if cent amount is greater than 0.2 AND less than 0.8
   then update cent amount to 0.5
   then add whole num to cent amount
 else
