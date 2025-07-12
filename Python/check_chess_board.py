9a14cff556bf9bb4ce0246089b630d693df13766
Stephany Ikebudu <stephikebudu@gmail.com>
2025-07-12 13:45:28 +0100

    implement input validation for challnge

--- Python/check_chess_board.py
+++ Python/check_chess_board.py
@@ -26,3 +26,26 @@ asterisk => black
 dot => white
 '''
 
+def check_chess_board(str):
+  if len(str) > 2:
+    return "Please enter one letter for column and one number for row on the board"
+
+  if str[0].isalpha():
+    if re.search(r"[a-h]", str[0]):
+      letter = str[0].lower()
+      number = int(str[1]) if int(str[1]) > 0 and int(str[1]) < 9 else "no value"
+    else:
+      return "Please enter a valid letter between a and h (both inclusive) to represent the board column"
+  elif isinstance(int(str[0]), int):
+    if int(str[0]) > 0 and int(str[0]) < 9:
+      number = int(str[0])
+      letter = re.search(r"[a-h]", str[1].lower()) and str[1] or  "no value"
+    else:
+      return "Please enter a valid number between 1 and 8 (both inclusive) to represent the board row"
+  else:
+    return "Please enter one valid letter (a-h) for column and one valid number (1-8) for row on the board"
+
+  if letter == "no value" or number == "no value":
+    return "Please enter one valid letter (a-h) for column and one valid number (1-8) for row on the board"
+
+  pass
