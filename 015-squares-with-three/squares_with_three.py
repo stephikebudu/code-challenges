d32152a1180509bbd3780daa9effb23d65629c5c
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-12 09:50:18 +0100

    docs: write py pseudocode for squares with three counter

---
+++ 015-squares-with-three/squares_with_three.py
@@ -0,0 +1,8 @@
+'''
+Pseudocode
+1. validate input
+2. init list to store total number of squares with three
+3. check through range - 1 up to num
+  4. if any num squared has 3, add it to total list initialized above
+5. count and return number of list elements
+'''
