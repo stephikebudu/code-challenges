d1e81face4cc6da916a3a707b77a76c17e0ec852
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-12 09:51:09 +0100

    feat: solve squares with three challenge in py, include tests

--- 015-squares-with-three/squares_with_three.py
+++ 015-squares-with-three/squares_with_three.py
@@ -6,3 +6,21 @@ Pseudocode
   4. if any num squared has 3, add it to total list initialized above
 5. count and return number of list elements
 '''
+
+def squares_with_three(num):
+  if num < 1 or num > 10000:
+    return "Please enter a number in the range 1 - 10000 (both inclusive)"
+
+  total_squares_with_three = []
+  for i in range(num):
+    if "3" in str(i ** 2):
+      total_squares_with_three.append(i)
+
+  return len(total_squares_with_three)
+
+
+print(squares_with_three(1)); # returns 0
+print(squares_with_three(10)); # returns 1
+print(squares_with_three(100)); # returns 19
+print(squares_with_three(1000)); # returns 326
+print(squares_with_three(10000)); # returns 4531
