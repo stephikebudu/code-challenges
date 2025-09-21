2a9da227ae4939fc1474ff308d1e5baf08996c38
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-21 21:11:45 +0100

    feat: solve mile pace challenge in js

--- 016-mile-pace/milePace.js
+++ 016-mile-pace/milePace.js
@@ -4,3 +4,27 @@ Pseudocode
 2. divide time by mile given
 3. transform answeer to minutes:seconds format and return as answer
 */
+
+function milePace(miles, time) {
+  let avgTime, newMin, newSec;
+
+  if (miles === 1) {
+    avgTime = time
+  } else {
+    const timeArr = time.split(":")
+    let timeToSec = (Number(timeArr[0]) * 60) + Number(timeArr[1]);
+    newMin = String(Math.floor((timeToSec / miles) / 60));
+    newSec = String(Math.round((timeToSec / miles) % 60));
+    if (newMin.length < 2) {
+      newMin = "0" + newMin;
+    }
+
+    if (newSec < 2) {
+      newSec = newSec + "0";
+    }
+
+    avgTime = newMin + ":" + newSec;
+  }
+
+  return avgTime;
+}
