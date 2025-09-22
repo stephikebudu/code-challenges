ad2332a0c726a6d02ff881402d12e5428ed5be5d
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-22 18:39:23 +0100

    pseudocode for mile pace challenge in py

---
+++ 016-mile-pace/mile_pace.py
@@ -0,0 +1,8 @@
+'''
+1. Split and transform input time to number format (in seconds)
+2. Calculate total time (in sec) divided by mile to get avg minutes
+3. Calculate total time (in sec) modulo mile to get avg seconds
+Remember to round down both values to nearest whole number and transform back to string after calc
+4. Check if the min or sec str len is 1, if yes, pad with 0 appropriately
+5. Combine and return final str
+'''
