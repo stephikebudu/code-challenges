c9ee67de0a8bc417f34233cefae47a9f56c5661f
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-24 17:49:06 +0100

    code cleanup - remove unused var, correct typos

--- 017-targeted-sum/targetedSum.js
+++ 017-targeted-sum/targetedSum.js
@@ -1,7 +1,7 @@
 /*
 Pseudocode
 1. Loop list, assign first element as assumed first num in sum
-2. Cheeck if the num with the sum of any othr numbr in the list equals the target num
+2. Cheeck if the num with the sum of any othr number in the array equals the target num
     If yes, return index of both in array
 3. Sort array of indices
 */
@@ -10,7 +10,6 @@ function findTarget(arr, target) {
   let firstNumIndex, secondNumIndex;
 
   for (let i = 0; i < arr.length; i++){
-    let firstNum = arr[i];
     if (arr[i] + arr[i + 1] == target) {
       firstNumIndex = i
       secondNumIndex = i + 1
@@ -20,7 +19,7 @@ function findTarget(arr, target) {
   if (typeof (firstNumIndex) !== "number") {
     indices = "Target not found";
   } else {
-    indices = [firstNumIndex, secondNumIndex];
+    indices = [firstNumIndex, secondNumIndex]; // sort indices arr
   }
   return indices;
 }
