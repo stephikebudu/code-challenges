3da4bb8e65fbf190e3a9605d2459b7b739486136
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-24 17:41:51 +0100

    run tests for targeted sum js logic

--- 017-targeted-sum/targetedSum.js
+++ 017-targeted-sum/targetedSum.js
@@ -24,3 +24,8 @@ function findTarget(arr, target) {
   }
   return indices;
 }
+
+console.log(findTarget([2, 7, 11, 15], 9)); // returns [0, 1]
+console.log(findTarget([3, 2, 4, 5], 6)); // returns [1, 2]
+console.log(findTarget([1, 3, 5, 6, 7, 8], 15)); // returns [4, 5]
+console.log(findTarget([1, 3, 5, 7], 14)); // returns "Target not found"
