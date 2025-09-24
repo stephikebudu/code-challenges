522d90e542acb788d6a114ffe0ebbcae66f00490
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-24 17:49:38 +0100

    pseudocode for targetd sum problem in py

---
+++ 017-targeted-sum/targeted_sum.py
@@ -0,0 +1,7 @@
+'''
+Pseudocode
+1. Loop list and assign first list element to first_num variable
+2. Check if sum of first_num and any other num in list equals target
+    If yes, return index of both
+3. Ensure the indices are in a sorted list
+'''
