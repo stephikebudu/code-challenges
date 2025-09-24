42a83fa6f3171bcd1996a6ae84977521ba593587
Stephany Ikebudu <stephikebudu@gmail.com>
2025-09-24 17:21:39 +0100

    pseudocode for targeted sum problem in js

---
+++ 017-targeted-sum/targetedSum.js
@@ -0,0 +1,7 @@
+/*
+Pseudocode
+1. Loop list, assign first element as assumed first num in sum
+2. Cheeck if the num with the sum of any othr numbr in the list equals the target num
+    If yes, return index of both in array
+3. Sort array of indexes
+*/
