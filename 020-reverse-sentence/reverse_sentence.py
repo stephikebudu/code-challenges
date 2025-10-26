93c310f716edba9384293871be266af74fe3c2aa
Stephany Ikebudu <stephikebudu@gmail.com>
2025-10-26 14:01:24 +0100

    test: run test cases

--- 020-reverse-sentence/reverse_sentence.py
+++ 020-reverse-sentence/reverse_sentence.py
@@ -3,3 +3,8 @@ def reverse_sentence(input_str):
   str_list.reverse()
   new_str = " ".join(str_list)
   return new_str
+
+print(reverse_sentence("world hello"))
+print(reverse_sentence("push commit git"))
+print(reverse_sentence("npm install sudo"))
+print(reverse_sentence("import default function export"))
