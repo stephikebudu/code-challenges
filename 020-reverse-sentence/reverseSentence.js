468e67525e9f4df7cd6139bd66141afb7ff36765
Stephany Ikebudu <stephikebudu@gmail.com>
2025-10-26 13:32:30 +0100

    test: run test cases

--- 020-reverse-sentence/reverseSentence.js
+++ 020-reverse-sentence/reverseSentence.js
@@ -2,3 +2,8 @@ function reverseSentence(str) {
   const newStr = str.split(" ").reverse().join(" ");
   return newStr
 }
+
+console.log(reverseSentence("world hello"))
+console.log(reverseSentence("push commit git"))
+console.log(reverseSentence("npm install sudo"))
+console.log(reverseSentence("import default function export"))
