9ead44e18ec1fb8a8cd782562be69f982da9a23a
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-03 00:02:52 +0100

    fix: update logic to resolve missing period bug

--- 021-longest-word/getLongestWord.js
+++ 021-longest-word/getLongestWord.js
@@ -1,11 +1,11 @@
 /*
 1. Remove period from sentence
 2. Split sentence into words by whitespace
-3. Check for and return longeest word in sentennce
+3. Check for and return longest word in sentence
 */
 
 function getLongestWord(str) {
-  let refined = str.includes(".") ?  str.split(".") : str;
+  let refined = str.includes(".") ? str.split(".") : str.split();
   let words = refined[0].split(" ");
   let longest = words[0];
   for (let i = 1; i < words.length; i++) {
@@ -14,9 +14,9 @@ function getLongestWord(str) {
     }
   }
 
-  return longest;
+  console.log(longest);
 }
 
-getLongestWord("coding is fun.");
+getLongestWord("coding is fun");
 getLongestWord("Coding challenges are fun and educational.");
 getLongestWord("This sentence has multiple long words.");
