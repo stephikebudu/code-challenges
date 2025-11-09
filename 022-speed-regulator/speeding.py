2a8f9ace66f61b00305330ca4682ffa6da82bcd5
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-10 00:04:37 +0100

    test: run test cases for py soln

--- 022-speed-regulator/speeding.py
+++ 022-speed-regulator/speeding.py
@@ -15,3 +15,9 @@ def speeding(speeds, limit):
   info = [0, 0] if len(over_limits) == 0 else [1, over_limits[0] - limit] if len(over_limits) == 1 else [len(over_limits), avg - limit]
   print(info)
   return
+
+speeding([50, 60, 55], 60)
+speeding([58, 50, 60, 55], 55)
+speeding([61, 81, 74, 88, 65, 71, 68], 70)
+speeding([100, 105, 95, 102], 100)
+speeding([40, 45, 44, 50, 112, 39], 55)
