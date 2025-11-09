d90445b081e93504419aed616ce5a13ccde37ea5
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-10 00:03:52 +0100

    feat: solve speed reg problem in py

--- 022-speed-regulator/speeding.py
+++ 022-speed-regulator/speeding.py
@@ -4,3 +4,14 @@
 3. Calculate difference between speed limit and cars that are over speed limit and return as 2nd element in answer list
 4. REM to account for cases where no car or 1 car is over speed limit
 '''
+
+def speeding(speeds, limit):
+  over_limits = []
+  for speed in speeds:
+    if speed > limit:
+      over_limits.append(speed)
+
+  avg = sum(over_limits)/len(over_limits) if len(over_limits) > 1 else None
+  info = [0, 0] if len(over_limits) == 0 else [1, over_limits[0] - limit] if len(over_limits) == 1 else [len(over_limits), avg - limit]
+  print(info)
+  return
