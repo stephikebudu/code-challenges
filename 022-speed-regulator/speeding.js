447c921b1eb6df1692118793b38a18b64f6dd042
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-09 23:46:39 +0100

    test: run tests for js soln

--- 022-speed-regulator/speeding.js
+++ 022-speed-regulator/speeding.js
@@ -9,10 +9,16 @@ function speeding(speeds, limit) {
   let overLimits = speeds.filter(speed => speed > limit);
   let avg;
   if (overLimits.length > 1) {
-    avg = (overLimits.reduce((a, b) => a + b, 0)) / overLimits.length;
+    avg = (overLimits.reduce((a, b) => a + b, 0))/overLimits.length;
   } else if (overLimits.length == 1) {
     avg = overLimits[0];
   }
   let info = overLimits.length == 0 ? [0, 0] : [overLimits.length, avg - limit];
   console.log(info);
 }
+
+speeding([50, 60, 55], 60)
+speeding([58, 50, 60, 55], 55)
+speeding([61, 81, 74, 88, 65, 71, 68], 70)
+speeding([100, 105, 95, 102], 100)
+speeding([40, 45, 44, 50, 112, 39], 55)
