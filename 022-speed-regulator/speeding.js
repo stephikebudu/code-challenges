fef2892260551af4e4ebdb4991754946976b4f8f
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-09 23:46:17 +0100

    feat: solve speed reg problem in js

--- 022-speed-regulator/speeding.js
+++ 022-speed-regulator/speeding.js
@@ -4,3 +4,15 @@
 3. Calculate difference between speed limit and cars that are over speed limit and return as 2nd element in answer array
 4. REM to account for cases where no car or 1 car is over speed limit
 */
+
+function speeding(speeds, limit) {
+  let overLimits = speeds.filter(speed => speed > limit);
+  let avg;
+  if (overLimits.length > 1) {
+    avg = (overLimits.reduce((a, b) => a + b, 0)) / overLimits.length;
+  } else if (overLimits.length == 1) {
+    avg = overLimits[0];
+  }
+  let info = overLimits.length == 0 ? [0, 0] : [overLimits.length, avg - limit];
+  console.log(info);
+}
