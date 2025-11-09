b54b65f5d3439cb83975287f87d0341bfee0277b
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-10 00:03:22 +0100

    doc: add py pseudocode

---
+++ 022-speed-regulator/speeding.py
@@ -0,0 +1,6 @@
+'''
+1. Identify vehicles over speed limit and put in list
+2. Get total number and return as first element in answer list
+3. Calculate difference between speed limit and cars that are over speed limit and return as 2nd element in answer list
+4. REM to account for cases where no car or 1 car is over speed limit
+'''
