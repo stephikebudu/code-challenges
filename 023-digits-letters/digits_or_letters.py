1093ae12964b912a4e4a5bdd6a128c3f227bcd77
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-17 15:45:59 +0100

    doc: add pseudocode for digits/letters challenge in py

---
+++ 023-digits-letters/digits_or_letters.py
@@ -0,0 +1,8 @@
+import re
+
+'''
+Pseudocode
+1. Use regex to check for digits/letters,
+2. Store all letters/digits found in str in list arr
+3. Compare length of both lists, if digits/letters are more or both are equal and return appropriate ans
+'''
