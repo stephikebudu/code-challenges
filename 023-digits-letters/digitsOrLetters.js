2a211691c3b603ac0670fb3ea1371d73a3cb80a8
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-17 15:31:24 +0100

    feat: solve digits/letters challenge in js

--- 023-digits-letters/digitsOrLetters.js
+++ 023-digits-letters/digitsOrLetters.js
@@ -4,3 +4,13 @@ Pseudocode
 2. Filter letters/numbers into an arr
 3. Compare length of both arrays, if digits/letters are more or both are equal and return appropriate ans
 */
+
+function digitsOrLetters(str) {
+  const digReg = /[0-9]/;
+  const letReg = /[a-z]/i;
+  const arr = str.split("");
+  const letters = arr.filter(i => letReg.test(i));
+  const nums = arr.filter(i => digReg.test(i));
+  let answer = letters.length > nums.length ? "letters" : nums.length > letters.length ? "digits" : "tie"
+  console.log(answer);
+}
