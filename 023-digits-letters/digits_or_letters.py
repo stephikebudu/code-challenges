1a744c8ac41f67910562890b9dc8c651f14f13c8
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-17 15:46:36 +0100

    feat: solve digits/letters challenge in py

--- 023-digits-letters/digits_or_letters.py
+++ 023-digits-letters/digits_or_letters.py
@@ -6,3 +6,11 @@ Pseudocode
 2. Store all letters/digits found in str in list arr
 3. Compare length of both lists, if digits/letters are more or both are equal and return appropriate ans
 '''
+
+def digits_or_letters(text):
+  dig_reg = r"[0-9]"
+  let_reg = r"[a-zA-Z]"
+  letters = re.findall(let_reg, text)
+  nums = re.findall(dig_reg, text)
+  answer = "letters" if len(letters) > len(nums) else "digits" if len(nums) > len(letters) else "tie"
+  print(answer)
