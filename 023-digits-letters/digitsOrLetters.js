5fcc48bb494189b4cc45f9159204765729eacf7c
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-17 15:29:23 +0100

    doc: js pseudocode for soln

---
+++ 023-digits-letters/digitsOrLetters.js
@@ -0,0 +1,6 @@
+/*
+Pseudocode
+1. Use regex to count no of digits and no of letters, store values in var
+2. Filter letters/numbers into an arr
+3. Compare length of both arrays, if digits/letters are more or both are equal and return appropriate ans
+*/
