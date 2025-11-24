3cc219fc9ebb3589dd70897463f6912a5e515b2d
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-24 21:59:27 +0100

    feat: solve thermostat adjuster in py

--- 024-thermostat-adjuster/adjust_thermostat.py
+++ 024-thermostat-adjuster/adjust_thermostat.py
@@ -2,3 +2,7 @@
 Pseudocode
 return appropriate string based on if current temp is greater, less or equivalent to target temp
 '''
+
+def adjust_thermostat(current, target):
+  ans = "heat" if current < target else ("cool" if current > target else ("hold" if current == target else ""))
+  print(ans)
