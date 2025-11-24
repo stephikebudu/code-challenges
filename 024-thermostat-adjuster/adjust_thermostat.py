32e420d37133d179e645eb61f913552aaa2629a2
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-24 21:59:57 +0100

    test: perform unit test for py soln

--- 024-thermostat-adjuster/adjust_thermostat.py
+++ 024-thermostat-adjuster/adjust_thermostat.py
@@ -6,3 +6,10 @@ return appropriate string based on if current temp is greater, less or equivalen
 def adjust_thermostat(current, target):
   ans = "heat" if current < target else ("cool" if current > target else ("hold" if current == target else ""))
   print(ans)
+
+adjust_thermostat(68, 72)
+adjust_thermostat(75, 72)
+adjust_thermostat(72, 72)
+adjust_thermostat(-20.5, -10.1)
+adjust_thermostat(100, 99.9)
+adjust_thermostat(0.0, 0.0)
