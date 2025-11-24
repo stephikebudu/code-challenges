1fb598e1b33338ae34a67ef16279f2a31cf65b65
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-24 21:48:39 +0100

    test: run unit test for js soln

--- 024-thermostat-adjuster/adjustThermostat.js
+++ 024-thermostat-adjuster/adjustThermostat.js
@@ -7,3 +7,10 @@ function adjustThermostat(roomTemp, targetTemp) {
   let ans = roomTemp < targetTemp ? "heat" : roomTemp > targetTemp ? "cool" : roomTemp == targetTemp ? "hold" : "Please enter valid temperatures."
   console.log(ans);
 }
+
+adjustThermostat(68, 72);
+adjustThermostat(75, 72);
+adjustThermostat(72, 72);
+adjustThermostat(-20.5, -10.1);
+adjustThermostat(100, 99.9);
+adjustThermostat(0.0, 0.0);
