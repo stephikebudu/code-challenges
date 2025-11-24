9acfa93f173bb8719448927a0c26c76f76ed9e64
Stephany Ikebudu <stephikebudu@gmail.com>
2025-11-24 21:48:01 +0100

    feat: solve thermostat adjuster problem in js

--- 024-thermostat-adjuster/adjustThermostat.js
+++ 024-thermostat-adjuster/adjustThermostat.js
@@ -2,3 +2,8 @@
 Pseudocode
 return appropriate string based on if current temp is greater, less or equivalent to target temp
 */
+
+function adjustThermostat(roomTemp, targetTemp) {
+  let ans = roomTemp < targetTemp ? "heat" : roomTemp > targetTemp ? "cool" : roomTemp == targetTemp ? "hold" : "Please enter valid temperatures."
+  console.log(ans);
+}
