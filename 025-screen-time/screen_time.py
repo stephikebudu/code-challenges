a976b28afbbaf0146ca9c71eba2a26f068e9f30f
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-08 21:28:28 +0100

    feat: code py soln for screen time challenge

--- 025-screen-time/screen_time.py
+++ 025-screen-time/screen_time.py
@@ -7,3 +7,10 @@ Pseudocode
     IF NOT condition, continue
 4. Return answer
 '''
+
+def too_much_screen_time(hour_list):
+  answer = False
+  for index, element in enumerate(hour_list):
+    if element >= 10 or index < 5 and ((hour_list[index] + hour_list[index + 1] + hour_list[index + 2]) / 3) >= 8 or (sum(hour_list) / 7) >= 6:
+      answer = True
+  return answer
