781004e79229e4ce2ca871affda8b38bf029d33e
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-08 21:10:41 +0100

    doc: add js pseudocode for screentime challenge

---
+++ 025-screen-time/screenTime.js
@@ -0,0 +1,9 @@
+/*
+Pseudocode
+1. Init answer var as "false"
+2. Loop through array
+3. Define conditions according to constraints
+    IF condition, update answer to true
+    IF NOT condition, continue
+4. Return answer
+*/
