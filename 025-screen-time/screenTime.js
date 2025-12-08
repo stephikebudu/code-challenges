5891b80566f6dcefefc407a94dbcca1a06b13b46
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-08 21:11:18 +0100

    feat: solve screen time problem in js

--- 025-screen-time/screenTime.js
+++ 025-screen-time/screenTime.js
@@ -7,3 +7,16 @@ Pseudocode
     IF NOT condition, continue
 4. Return answer
 */
+
+function tooMuchScreenTime(arr) {
+  const arrSum = arr.reduce((a, b) => a + b);
+  let answer = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 10 || i < 5 && ((arr[i] + arr[i + 1] + arr[i + 2]) / 3) >= 8 || (arrSum / 7) >= 6) {
+      answer = true;
+    }
+  }
+
+  return answer;
+}
