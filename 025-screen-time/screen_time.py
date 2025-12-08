35d3ddac8e44e1e8406ed2b4b331138066f1ad13
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-08 21:27:49 +0100

    doc: pseudocode for py soln

---
+++ 025-screen-time/screen_time.py
@@ -0,0 +1,9 @@
+'''
+Pseudocode
+1. Init answer var as "False"
+2. Loop through array
+3. Define conditions according to constraints
+    IF condition, update answer to "True"
+    IF NOT condition, continue
+4. Return answer
+'''
