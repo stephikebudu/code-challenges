17810ea4738973d54cf456f858d2d3d11380a31b
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-15 20:37:35 +0100

    feat: solve challenge in py

--- 026-slug-generator/generate_slug.py
+++ 026-slug-generator/generate_slug.py
@@ -1,5 +1,13 @@
+import re
+
 '''
 1. Clean string - lowercase and trim string
 2. Filter characters with regex
 3. Convert whitespace character to "%20" and return final transformed text
 '''
+
+def generate_slug(text):
+  not_allowed = r"[^\w\s]"
+  clean_text = text.lower().strip()
+  slug = re.sub(not_allowed, "", clean_text)
+  return re.sub(" ", "%20", slug)
