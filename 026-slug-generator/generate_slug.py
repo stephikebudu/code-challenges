da66f6ad933f7c46c5bf6d8cb7fddfd63479f0d6
Stephany Ikebudu <stephikebudu@gmail.com>
2025-12-15 20:38:07 +0100

    test: run tests for py soln

--- 026-slug-generator/generate_slug.py
+++ 026-slug-generator/generate_slug.py
@@ -11,3 +11,9 @@ def generate_slug(text):
   clean_text = text.lower().strip()
   slug = re.sub(not_allowed, "", clean_text)
   return re.sub(" ", "%20", slug)
+
+print(generate_slug("helloWorld"))
+print(generate_slug("hello world!"))
+print(generate_slug(" hello-world "))
+print(generate_slug("hello world") )
+print(generate_slug(" ?H^3-1*1]0! W[0%R#1]D "))
